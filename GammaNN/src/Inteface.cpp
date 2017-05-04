3085cbf2ca13f7c1862de4651e5495222613febc
Unknown <sahlet@yandex.ru>
2017-05-04 10:48:06 +0300

    commit

--- GammaNN/src/Inteface.cpp
+++ GammaNN/src/Inteface.cpp
@@ -1,15 +1,17 @@
 // [[Rcpp::plugins(cpp11)]]
 
 #include <Rcpp.h>
-//#include "My/Perceptron.cpp"
+#include <My/Perceptron.h>
 
 // [[Rcpp::export]]
 Rcpp::List rcpp_hello() {
 
-  //My::Perceptron p(3);
+  My::Perceptron p(3);
 
   auto x = Rcpp::CharacterVector::create("foo", "bar");
-  auto y = Rcpp::NumericVector::create(0.0, my_func());
+  auto y = Rcpp::NumericVector::create(0.0, 1);
   auto z = Rcpp::List::create(x, y);
+
+
   return z;
 }
